2444b2c9ac90c2e56ae40ea0008dcdc35ca2d021
davidjnovak <51279385+davidjnovak@users.noreply.github.com>
2023-07-02 16:58:26 -0400

    converting Game.js to a class

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,185 +1,202 @@
-import React,{useRef, useEffect} from "react";
+import React, { Component } from "react";
+import textData from "../data/data.json";
 
-const textData = require("../data/data.json")
+class Game extends Component {
+  constructor(props) {
+    super(props);
 
-const Input = () => {
-    const userInput = useRef(null);
-    const NUMBER_OF_TEXTS = 7411;
-    var text = chooseRandomText();
-    var textString = "";
-    var targetWord = text[0];
-    var textComplete = false;
-    var started = false;
-    var startTime;
-    var typedWords = 0;
-    var wpm = 0;
+    this.userInput = React.createRef();
+    this.NUMBER_OF_TEXTS = 7411;
+    this.text = this.chooseRandomText();
+    this.textString = "";
+    this.targetWord = this.text[0];
+    this.textComplete = false;
+    this.started = false;
+    this.typedWords = 0;
+    this.startTime = null;
+    this.wpm = 0;
+  }
 
-    function updateText(text){
-        document.getElementById('quote').innerHTML = text;
+  updateText(text) {
+    document.getElementById("quote").innerHTML = text;
+  }
+
+  updateScore(score) {
+    document.getElementById("score").innerHTML = score;
+  }
+
+  textOfTheDayButtonClick = () => {
+    this.text = this.fetchQuoteOfTheDay();
+    this.updateText(this.updateDisplayString());
+    this.targetWord = this.text[0];
+    this.started = false;
+    this.typedWords = 0;
+    this.wpm = 0;
+  };
+
+  newTextButtonClick = () => {
+    this.text = this.chooseRandomText();
+    this.updateText(this.updateDisplayString());
+    this.targetWord = this.text[0];
+    this.started = false;
+    this.typedWords = 0;
+    this.wpm = 0;
+  };
+
+  getDailyIndex() {
+    const currentDate = new Date().toLocaleDateString();
+    const totalTexts = Object.keys(textData).length;
+
+    let seed = 0;
+    console.log(currentDate);
+    for (let i = 0; i < currentDate.length; i++) {
+      seed += currentDate.charCodeAt(i);
+      console.log(currentDate.charCodeAt(i));
     }
+    const randomIndex = Math.floor((seed % totalTexts) + 1);
+    return randomIndex;
+  }
 
-    function updateScore(score){
-        document.getElementById('score').innerHTML = score;
-    }
-
-    function textOfTheDayButtonClick(){
-        text = fetchQuoteOfTheDay();
-        updateText(updateDisplayString());
-        targetWord = text[0];
-        started = false;
-        typedWords = 0;
-        wpm = 0;
-    }
-
-    function newTextButtonClick(){
-        text = chooseRandomText();
-        updateText(updateDisplayString());
-        targetWord = text[0];
-        started = false;
-        typedWords = 0;
-        wpm = 0;
-    }
-
-    function getDailyIndex() {
-        const currentDate = new Date().toLocaleDateString();
-        const totalTexts = Object.keys(textData).length;
-      
-        let seed = 0;
-        console.log(currentDate);
-        for (let i = 0; i < currentDate.length; i++) {
-          seed += currentDate.charCodeAt(i);
-          console.log(currentDate.charCodeAt(i));
-        }
-        const randomIndex = Math.floor((seed % totalTexts) + 1);
-        return randomIndex;
+  fetchQuoteOfTheDay() {
+    this.textComplete = false;
+    this.started = false;
+    var imported = textData[this.getDailyIndex()][0];
+    var wordList = imported.split(" ");
+    const lastWordIndex = wordList.length - 1;
+    const updatedWordList = wordList.map((word, index) => {
+      if (index !== lastWordIndex) {
+        return word + " ";
       }
-      
-    function fetchQuoteOfTheDay(){
-        textComplete = false;
-        started = false;
-        var imported = textData[getDailyIndex()][0];
-        var wordList = imported.split(" ");
-        const lastWordIndex = wordList.length - 1;
-        const updatedWordList = wordList.map((word, index) => {
-          if (index !== lastWordIndex) {
-            return word + " ";
-          }
-          return word;
-        });
-        return updatedWordList
+      return word;
+    });
+    return updatedWordList;
+  }
+
+  chooseRandomText() {
+    this.textComplete = false;
+    this.started = false;
+    const difficultyThreshold = 0.2;
+    var textDifficulty;
+    var index;
+    index = Math.floor(Math.random() * this.NUMBER_OF_TEXTS);
+    // while(textDifficulty < difficultyThreshold){
+    //     index = Math.floor(Math.random() * NUMBER_OF_TEXTS);
+    //     textDifficulty = textData[index][1]
+    // }
+    var imported = textData[index][0];
+    var wordList = imported.split(" ");
+    const lastWordIndex = wordList.length - 1;
+    const updatedWordList = wordList.map((word, index) => {
+      if (index !== lastWordIndex) {
+        return word + " ";
+      }
+      return word;
+    });
+    return updatedWordList;
+  }
+
+  updateDisplayString() {
+    this.textString = "";
+    for (const word in this.text) {
+      this.textString += this.text[word];
     }
+    return this.textString;
+  }
 
+  calculateWPM(endTime) {
+    return (60000 * this.typedWords) / (parseFloat(endTime) - this.startTime);
+  }
 
-    function chooseRandomText(){
-        textComplete = false
-        started = false;
-        const difficultyThreshold = .2;
-        var textDifficulty;
-        var index;   
-        index = Math.floor(Math.random() * NUMBER_OF_TEXTS);     
-        // while(textDifficulty < difficultyThreshold){ 
-        //     index = Math.floor(Math.random() * NUMBER_OF_TEXTS);
-        //     textDifficulty = textData[index][1]
-        // }
-        var imported = textData[index][0];
-        var wordList = imported.split(" ");
-        const lastWordIndex = wordList.length - 1;
-        const updatedWordList = wordList.map((word, index) => {
-          if (index !== lastWordIndex) {
-            return word + " ";
-          }
-          return word;
-        });
-        return updatedWordList
+  handleInput = () => {
+    const typed = this.userInput.current.value;
+    const style = this.userInput.current.style;
+
+    if (this.textComplete) {
+      return;
     }
-
-    function updateDisplayString(){
-        textString = ""
-        for (const word in text){
-            textString += text[word]
-        }
-        return textString;
+    if (this.started === false) {
+      this.started = true;
+      this.startTime = performance.now();
+      this.typedWords = 0;
+    } else if (typed === this.text[this.text.length - 1]) {
+      this.typedWords += 1;
+      this.wpm = this.calculateWPM(performance.now());
+      this.updateScore(this.wpm.toFixed(1) + " wpm");
+      this.textComplete = true;
+      this.text.splice(0, 1);
+      this.userInput.current.value = "";
+      this.updateDisplayString();
+      this.updateText("");
     }
-
-    function calculateWPM(endTime){
-        return (60000*typedWords/(parseFloat(endTime)-startTime));
+    //word has been typed correctly -- remove it from array, change target word
+    else if (this.targetWord === typed) {
+      this.typedWords += 1;
+      this.wpm = this.calculateWPM(performance.now());
+      this.updateScore("speed: " + this.wpm.toFixed(1));
+      this.text.splice(0, 1);
+      this.userInput.current.value = "";
+      this.targetWord = this.text[0];
+      this.updateDisplayString();
+      this.updateText(this.textString);
+    } else if (this.targetWord.substring(0, typed.length) === typed) {
+      this.userInput.current.sytle = "";
+      style.backgroundColor = "antiquewhite";
+    } else {
+      style.backgroundColor = "#f47174";
     }
+  };
 
-    function handleInput(){
-        const typed = userInput.current.value
-        const style = userInput.current.style
+  componentDidMount() {
+    const handleKeyPress = (e) => {
+      this.userInput.current.focus();
+    };
+    document.addEventListener("keypress", handleKeyPress);
 
-        if(textComplete){
-            return;
-        }
-        if (started === false){
-            started = true;
-            startTime = performance.now();
-            typedWords = 0
-        }
-        else if (typed === text[text.length-1]){
-            typedWords += 1
-            wpm = calculateWPM(performance.now());
-            updateScore(wpm.toFixed(2) + " wpm");
-            textComplete = true;
-            text.splice(0,1)
-            userInput.current.value = "";
-            updateDisplayString();
-            updateText("");
-        }
-        //word has been typed correctly -- remove it from array, change target word
-        else if(targetWord === typed){
-            typedWords += 1;
-            wpm = calculateWPM(performance.now());
-            updateScore("speed: " + wpm.toFixed(2));
-            text.splice(0,1)
-            userInput.current.value = "";
-            targetWord = text[0];
-            updateDisplayString();
-            updateText(textString);
-        }
-        else if (targetWord.substring(0,typed.length) === typed){ 
-            userInput.current.sytle = ""
-            style.backgroundColor = "antiquewhite";
-        }
-        else{
-            style.backgroundColor = "#f47174";
-        }
-    }
+    this.cleanup = () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }
 
-    useEffect(() => {
-        function handleKeyPress(e) {
-          userInput.current.focus();
-        }
-        document.addEventListener("keypress", handleKeyPress);
-    
-        return function cleanup() {
-          document.removeEventListener("keypress", handleKeyPress);
-        };
-      }, []);
+  componentWillUnmount() {
+    this.cleanup();
+  }
 
+  render() {
     return (
-        <>
-        <div className="firstRow"> 
-            <div id="score" className="statContainer">{"speed: " + wpm}</div>
-            <div className='inputContainer'>
-                <input ref={userInput} id = "input" className = "inputBox" type="text" onChange={handleInput} />
-                <div className='textBox' id = "quote">{updateDisplayString()}</div>
+      <>
+        <div className="firstRow">
+          <div className="inputContainer">
+            <input
+              ref={this.userInput}
+              id="input"
+              className="inputBox"
+              type="text"
+              onChange={this.handleInput}
+            />
+            <div className="textBox" id="quote">
+              {this.updateDisplayString()}
             </div>
-            <div id="score" className="statContainer"> 
-                <button className='button' onClick={newTextButtonClick}>New Text</button>
-                <button className='button' onClick={textOfTheDayButtonClick}>Text of the Day</button>
+            <div id="score" className="statContainer">
+              {"speed: " + this.wpm}
             </div>
+          </div>
+          <div id="score" className="statContainer">
+            <button className="button" onClick={this.newTextButtonClick}>
+              New Text
+            </button>
+            <button
+              className="button"
+              onClick={this.textOfTheDayButtonClick}
+            >
+              Text of the Day
+            </button>
+          </div>
         </div>
 
-
-        <div tabIndex="0">
-        </div>
-        </>
-    )
+        <div tabIndex="0"></div>
+      </>
+    );
+  }
 }
-export default Input;
 
-
-
-
+export default Game;
